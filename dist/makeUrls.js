c2e8354c82a0f95ddffec29024a4e925cc7c9354
ben.ritchie <ben.ritchie@connect4.co.uk>
2021-02-24 13:05:42 +0000

    Updated dist with latest 'release'

--- dist/makeUrls.js
+++ dist/makeUrls.js
@@ -5,23 +5,30 @@ var makeDuration = function (event) {
     return "" + ("0" + Math.floor(minutes / 60)).slice(-2) + ("0" + minutes % 60).slice(-2);
 };
 var makeTime = function (time) { return new Date(time).toISOString().replace(/[-:]|\.\d{3}/g, ""); };
-var makeUrl = function (base, query) { return Object.keys(query).reduce(function (accum, key, index) {
-    var value = query[key];
-    if (value === null || value === undefined) {
-        return accum;
-    }
-    // if array then flatten down to csv
-    var stringValue = Array.isArray(value) ? value.join(",") : value;
-    return "" + accum + (index === 0 ? "?" : "&") + key + "=" + encodeURIComponent(stringValue);
-}, base); };
-var fixOutlookText = function (urlEncoded) {
-    urlEncoded.replace(" ", "%0A");
-    urlEncoded.replace("(", escape("("));
-    urlEncoded.replace(")", escape(")"));
-    return urlEncoded;
+var makeUrl = function (base, query, outlook) {
+    if (outlook === void 0) { outlook = false; }
+    return Object.keys(query).reduce(function (accum, key, index) {
+        var value = query[key];
+        if (value === null || value === undefined) {
+            return accum;
+        }
+        // if array then flatten down to csv
+        var stringValue = Array.isArray(value) ? value.join(",") : value;
+        // outlook didn't like our fix to space encoding used in fixOutlookText in location field
+        var encodedStringValue = outlook && key !== "location" ? fixOutlookText(encodeURIComponent(stringValue)) : encodeURIComponent(stringValue);
+        return "" + accum + (index === 0 ? "?" : "&") + key + "=" + encodedStringValue;
+    }, base);
 };
-var fixOutlookEmail = function (email) {
-    return email.replace("+", "+");
+var fixOutlookText = function (urlEncoded) {
+    // https://github.com/connect4app/add-to-calendar/issues/3
+    // https://github.com/InteractionDesignFoundation/add-event-to-calendar-docs/issues/20
+    urlEncoded = urlEncoded.replace(/%20/g, "%26%2332%3B");
+    // https://github.com/connect4app/add-to-calendar/issues/2
+    urlEncoded = urlEncoded.replace(/\(/g, escape("["));
+    urlEncoded = urlEncoded.replace(/\)/g, escape("]"));
+    // https://github.com/connect4app/add-to-calendar/issues/1
+    urlEncoded = urlEncoded.replace(/\+/g, escape("+"));
+    return urlEncoded;
 };
 // some unoffical references for URL format:
 // https://stackoverflow.com/questions/22757908/what-parameters-are-required-to-create-an-add-to-google-calendar-link
@@ -35,9 +42,17 @@ var makeGoogleCalendarUrl = function (event) { return makeUrl("https://calendar.
     add: event.addresses
 }); };
 var makeOutlookCalendarUrl = function (event) {
-    if (event.addresses) {
-        event.addresses.map(function (e) { return fixOutlookEmail(e); });
-    }
+    // if (event.addresses) {
+    //   event.addresses.map((e) => { return fixOutlookEmail(e) });
+    // }
+    //
+    // if (event.details) {
+    //   event.details = fixOutlookText(event.details);
+    // }
+    //
+    // if (event.name) {
+    //   event.name = fixOutlookText(event.name);
+    // }
     return makeUrl("https://outlook.live.com/calendar/0/deeplink/compose", {
         startdt: event.startsAt,
         enddt: event.endsAt,
@@ -47,7 +62,7 @@ var makeOutlookCalendarUrl = function (event) {
         to: event.addresses,
         allday: false,
         uid: new Date().getTime().toString(),
-    });
+    }, true);
 };
 // const makeOutlookCalendarUrl = (event: CalendarEvent) => {
 //   makeUrl("https://outlook.live.com/owa", {
